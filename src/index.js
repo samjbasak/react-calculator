fa70b93261411771a5a77dccb9fcd53c0fe35a06
samjbasak <sam.basak@wearecodenation.com>
2019-06-24 17:10:05 +0100

    Shows the correct value when multiple numbers are pressed

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ class Calc extends React.Component {
             interum = this.createObject(this.state.sumSoFar+value,value,'number')
         }
         else if (this.state.lastButtonType === 'number') {
-            interum = this.createObject(this.state.sumSoFar+value,value+value,'number')
+            interum = this.createObject(this.state.sumSoFar+value,this.state.valueShow+value,'number')
         }
         this.setState({
             sumSoFar: interum.sumSoFar,
@@ -83,22 +83,22 @@ class Calc extends React.Component {
         return (
             <div className="container">
                 <h3 className="box answer">{this.state.valueShow}</h3>
-                <h3 className="box clear" onClick={this.clear}>clear</h3>
-                <h3 className="box red" onClick={() => this.operator('/')}>/</h3>
-                <h3 className="box white" onClick={() => this.number('7')}>7</h3>
-                <h3 className="box white" onClick={() => this.number('8')}>8</h3>
-                <h3 className="box white" onClick={() => this.number('9')}>9</h3>
-                <h3 className="box red" onClick={() => this.operator('*')}>x</h3>
-                <h3 className="box white" onClick={() => this.number('4')}>4</h3>
-                <h3 className="box white" onClick={() => this.number('5')}>5</h3>
-                <h3 className="box white" onClick={() => this.number('6')}>6</h3>
-                <h3 className="box red" onClick={() => this.operator('-')}>-</h3>
-                <h3 className="box white" onClick={() => this.number('1')}>1</h3>
-                <h3 className="box white" onClick={() => this.number('2')}>2</h3>
-                <h3 className="box white" onClick={() => this.number('3')}>3</h3>
-                <h3 className="box red" onClick={() => this.operator('+')}>+</h3>
-                <h3 className="box white zero" onClick={() => this.number('0')}>0</h3>
-                <h3 className="box equals" onClick={this.equals}>=</h3>
+                <h3 className="box clear operator" onClick={this.clear}>clear</h3>
+                <h3 className="box operator" onClick={() => this.operator('/')}>/</h3>
+                <h3 className="box number" onClick={() => this.number('7')}>7</h3>
+                <h3 className="box number" onClick={() => this.number('8')}>8</h3>
+                <h3 className="box number" onClick={() => this.number('9')}>9</h3>
+                <h3 className="box operator" onClick={() => this.operator('*')}>x</h3>
+                <h3 className="box number" onClick={() => this.number('4')}>4</h3>
+                <h3 className="box number" onClick={() => this.number('5')}>5</h3>
+                <h3 className="box number" onClick={() => this.number('6')}>6</h3>
+                <h3 className="box operator" onClick={() => this.operator('-')}>-</h3>
+                <h3 className="box number" onClick={() => this.number('1')}>1</h3>
+                <h3 className="box number" onClick={() => this.number('2')}>2</h3>
+                <h3 className="box number" onClick={() => this.number('3')}>3</h3>
+                <h3 className="box operator" onClick={() => this.operator('+')}>+</h3>
+                <h3 className="box zero" onClick={() => this.number('0')}>0</h3>
+                <h3 className="box equals operator" onClick={this.equals}>=</h3>
             </div>
         )
     }
@@ -110,3 +110,12 @@ ReactDOM.render(<Calc />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+/*
+buttonArr.map (buttonVal => {
+    return <button name={buttonVal} onClick={e.target.name)}>{buttonVal}</button>
+})
+Use map on my buttons
+Split out my components
+*/
